381c2f8683695f72f3871e195e4824c933b7700a
Denys Vitali <denys.vitali@coelis.ch>
2017-09-24 17:22:59 +0200

    Support for playMp3Stream

--- src/js/inc/playManager.js
+++ src/js/inc/playManager.js
@@ -25,11 +25,38 @@ class PlayManager {
 
                 var durationString = hours + ':' + ('0' + minutes).substr(-2) + ':' + ('0' + seconds).substr(-2);
                 metadata.duration = durationString;
-                console.log(hours, minutes, seconds);
-
             }
         }
         this._socket.emit('playUrl', {
+            trackUrl: url,
+            metadata: metadata
+        });
+    }
+
+    playMp3Stream(url, metadata){
+        if (metadata === null) {
+            metadata = {
+                title: 'Title',
+                artist: 'Artist',
+                album: 'Album',
+                albumArt: '',
+                duration: 0
+            };
+        } else {
+            if (!isNaN(metadata.duration)) {
+                // duration is always in seconds
+                // need to convert it in H:MM:SS
+                var hours = Math.floor(metadata.duration / 3600);
+                var remaining = metadata.duration % 3600;
+                var minutes = Math.floor(remaining / 60);
+                remaining = remaining % 60;
+                var seconds = Math.floor(remaining);
+
+                var durationString = hours + ':' + ('0' + minutes).substr(-2) + ':' + ('0' + seconds).substr(-2);
+                metadata.duration = durationString;
+            }
+        }
+        this._socket.emit('playStreamUrl', {
             trackUrl: url,
             metadata: metadata
         });
