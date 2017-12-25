efdf26d0ae5e0279a9ca657034485c81919e9b53
Denys Vitali <denys@denv.it>
2017-12-25 11:56:14 +0100

    Update packages, Add Popular Music Videos to YouTube

--- plugins/youtube/main.js
+++ plugins/youtube/main.js
@@ -7,6 +7,7 @@
 	const moment = require('moment');
 	const request = require('request');
 	const rp = require('request-promise');
+	const util = require('util');
 	youtube.setKey('AIzaSyCEyw0VMAQBNTWZNZmEfb0DJDi0IA2Ew00');
 	class YouTube {
 		constructor(SonosWeb, settings) {
@@ -22,63 +23,113 @@
 			});
 
 			SonosWeb.app.get('/plugins/youtube/category/music', (req, res) => {
-				youtube.searchByChannel('UC-9-kyTW8ZkZNDHQJ6FgpwQ', 50, (error, result) => {
-					console.log('Got an answer [YT]');
+				let playlistPromises = [];
+				playlistPromises.push(new Promise((resolve) => {
+					youtube.getPlayListsById('PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI', (err, r)=>{
+						resolve(r.items);
+					});
+				}));
 
-					// Music
-					if (error || !result.hasOwnProperty('items')) {
-						console.log(error);
-						res.json({
-							success: false,
-							error: {
-								code: 1,
-								text: 'Unable to fetch Music channel'
-							}
-						});
-						return;
-					}
-					if (result.items.length === 0) {
-						res.json({
-							success: false,
-							error: {
-								code: 2,
-								text: 'No items'
-							}
-						});
-						return;
-					}
-					let promArr = [];
-					let createPromise = (i) => {
-						return new Promise((resolve) => {
-							youtube.getPlayListsItemsById(result.items[i].id.playlistId, (err, res) => {
-								if (!err) {
-									resolve({
-										name: result.items[i].snippet.title,
-										thumbnails: result.items[i].snippet.thumbnails,
-										description: result.items[i].snippet.description,
-										items: res.items
-									});
-								} else {
-									console.log(err);
-									resolve(null);
+				playlistPromises.push(new Promise((resolve) => {
+					youtube.searchByChannel('UC-9-kyTW8ZkZNDHQJ6FgpwQ', 50, (error, result) => {		
+						// Music
+						/*if (error || !result.hasOwnProperty('items')) {
+							console.log(error);
+							res.json({
+								success: false,
+								error: {
+									code: 1,
+									text: 'Unable to fetch Music channel'
 								}
 							});
-						});
-					};
-
-					for (let i = 0; i < result.items.length; i++) {
-						if (result.items[i].id.kind === 'youtube#playlist') {
-							let promise = createPromise(i);
-							promArr.push(promise);
-						}
-					}
-
-					Promise.all(promArr).then((data) => {
-						res.json(data);
+							return;
+						}*/
+						
+						resolve(result.items);
 					});
-				});
-			});
+				}));
+				
+				Promise.all(playlistPromises).then((data) => {
+					let promArr = [];
+					for(let i in data){
+						if(data.hasOwnProperty(i)){
+						let result = data[i];
 
+						let createPLPromise = (el) => {
+							return new Promise((resolve) => {
+								youtube.getPlayListsItemsById(el.id, 50, (err, res) => {
+									if (!err) {
+										resolve({
+											name: el.snippet.title,
+											thumbnails: el.snippet.thumbnails,
+											description: el.snippet.description,
+											items: res.items
+										});
+									} else {
+										console.log(err);
+										resolve(null);
+									}
+								});
+							});
+						};
+
+
+						if(result.hasOwnProperty('kind')){
+							if(result.kind == 'youtube#playlist'){
+								SonosWeb.warn(`Creating PL Promise from youtube#playlist`);
+								promArr.push(createPLPromise(result));
+							} else {
+								SonosWeb.warn(`Unhandled result: ${result.kind}`);
+							}
+						} else {
+							for(let i in result){
+								if(result[i].hasOwnProperty('kind')){
+									if(result[i].kind == 'youtube#searchResult'){
+										let searchResult = result[i];
+										if(searchResult.id.kind == 'youtube#playlist'){
+											promArr.push(createPLPromise({
+												id: searchResult.id.playlistId,
+												snippet: searchResult.snippet
+											}));
+										}
+									} else if(result[i].kind == 'youtube#playlist') {
+										let playlist = result[i];
+										promArr.push(createPLPromise({
+											id: playlist.id,
+											snippet: playlist.snippet
+										}));
+									} else {
+										SonosWeb.warn(`Unhandled kind: ${result[i].kind}!`);
+									}
+								}
+							}
+						}/*
+
+						if(result.kind === 'youtube#playlist'){
+							promArr.push(createPLPromise(result));
+						} else {
+							for (let i = 0; i < result.items.length; i++) {
+								if (result.items[i].id.kind === 'youtube#playlist') {
+									//let promise = createPromise(i);
+									promArr.push(createPLPromise(result.items[i]));
+								}
+							}
+						}
+						*/
+					}
+				}
+
+				Promise.all(promArr).then((data) => {
+					res.json(data);
+				}).catch((reason)=>{
+					SonosWeb.error(`[YT] Unable to fetch playlists. ${reason}`);
+				});
+
+				}).catch((reason)=>{
+					SonosWeb.error(`[YT] Can't fetch playlists. ${reason}, ${reason.stack}`);
+				})
+			});
+				
 			SonosWeb.app.get('/plugins/youtube/resolve/:videoid', (req, res) => {
 				let videoid = req.params.videoid.toString();
 				let matches = videoid.match(/^[0-9A-z\_\-]{11}$/i);
@@ -146,7 +197,7 @@
 
 			});
 
-			SonosWeb.app.get('/plugins/youtube/play/:videoid', (req, res) => {
+			SonosWeb.app.get('/plugins/youtube/play/:videoid', SonosWeb._middlewares.caching(60), (req, res) => {
 				let videoid = req.params.videoid.toString();
 				let matches = videoid.match(/^[0-9A-z\_\-]{11}$/i);
 				if (!matches) {
@@ -205,59 +256,50 @@
 					//video.pipe(fs.createWriteStream(__dirname + '/cached/' + videoid + '.mp4'));
 				} else {
 					res.setHeader('Content-Type', 'audio/mpeg');
-					let lbn = Math.round(Math.random()*7)+1; // Load balancing (d1 - d8) - client side
+					
+					let callback = 'jQuery32101618681714514778';
 
-					request.get('https://break.tv/video/'+videoid, (err,resp,body)=>{
-						let token = body.match(/'i':.*?'(.*?)'/)[1];					
+					request.get(`https://d.ymcdn.cc/check.php`, {
+						qs: {
+							'callback': 'jQuery32101618681714514778',
+							'_': '1512757100117',
+							'f': 'mp3',
+							'v' : videoid,
 
-						request.get(`http://d${lbn}.ytcore.org/dl/index.php`, {
-							qs: {
-								'idv': videoid,
-								'type': 'mp3',
-								'qu': '192 kbps',
-								'title': 'Title',
-								'server': `https://d${lbn}.ytcore.org/`,
-								'i': token
-							},
-							headers:{
-								'Connection': null,
-								'host': null,
-								'Host': 'd2.ytcore.org'
-							},
-							qsStringifyOptions: { format : 'RFC1738' }
-						}, (err, resp, body)=>{
-							if(err){
-								SonosWeb.debug(`Got an error: ${err}`);
-								res.end();
-								return;
-							}
-
-							if(body == 'access denied'){
-								SonosWeb.debug(`[YT - BreakTV] Server: ${lbn}`);
-								SonosWeb.error("[YT - BreakTV] Access denied!");
-								console.log(resp);
-								return;
-							}
-							
-							SonosWeb.debug("[YT - BreakTV] No errors");
-
-							let json = JSON.parse(body.match(/{.*?}/)[0]);
-							let jobid = json.success;
-
-							SonosWeb.debug("[YT - BreakTV] "+json.toString());
-								
-							if(jobid != false){
-								SonosWeb.info(`[YT - BreakTV] YT video is available, job is ${jobid}`);
-								res.setHeader('Content-Length', 1024 * 1024 * 1024 * 4); // What a bad workaround! We can stream up to 4GB
-								res.setHeader('Content-Type', 'audio/mpeg');
-								request.get(`http://d${lbn}.ytcore.org/sse/?jobid=${jobid}`).pipe(res);
-								return;
-							} else {
-								SonosWeb.warn(json);
-							}
-
+						},
+						headers: {
+							'Referer': 'https://ytmp3.cc/',
+							'X-Requested-With' : 'XMLHttpRequest',
+							'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:59.0) Gecko/20100101 Firefox/59.0'
+						}
+					}, (err, resp, body)=>{
+						if(err){
+							SonosWeb.debug(`Got an error: ${err}`);
 							res.end();
-						});
+							return;
+						}
+
+						let json;
+
+						try{
+							json = JSON.parse(body.replace(callback + '(', '').slice(0, -1));
+						} catch(e)
+						{
+							SonosWeb.error("[YT - EP] " + e);
+							console.log(body);
+							return;
+						}
+
+						if(json.error != ''){
+							console.error("[YT - EP] Message is not 'good': " + json.message);
+							return;
+						}
+
+						console.log(json);
+						
+						SonosWeb.debug("[YT - EP] No errors");	
+						SonosWeb.debug(`[YT - EP] Url is ${json.dlurl}`);
+						request.get(`https://tff.ymcdn.cc/${json.hash}/${videoid}`).pipe(res);
 					});
 				}
 
